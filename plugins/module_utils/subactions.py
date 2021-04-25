1de0f4150de99bf5ef8b5d6c50addd41d98905e0
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2021-04-25 11:43:34 +0200

    [feature] Optional `failed_when` callback to `query` and `change`
    Works like the equivalent feature in Ansible, except of course the
    callback is a full-fledged Python function not a Jinja string.
    
    - Rename _raise_if_failed into _redress_failure; keep it protected /
    overridable in subclasses
    
    - Returns an exception in case of failure instead of outright throwing
    it (so that bookkeeping can still happen in caller, which must then throw)

--- plugins/module_utils/subactions.py
+++ plugins/module_utils/subactions.py
@@ -52,7 +52,7 @@ class Subaction(object):
         except TypeError:
             init_old_calling_convention(self, *args, **kwargs)
 
-    def query (self, action_name, args):
+    def query (self, action_name, args, failed_when=None):
         """Execute a read-only Ansible sub-action.
 
         Only a cursory sanity check is performed on `action_name`; this
@@ -65,13 +65,18 @@ class Subaction(object):
 
         :param action_name: Ansible module name to use
         :param args: dict with arguments to give to module
+        :param failed_when: An optional function that takes the result and returns
+            a truthy value iff the action failed
         """
         def run_and_update_result(self):
             query_result = self.__ansible.run_action(action_name, args)
+            error = self._redress_failure(query_result, failed_when)
             if self.result is not None:
                 AnsibleResults.update(self.result, AnsibleResults.unchanged(query_result))
-            self._raise_if_failed(query_result)
-            return query_result
+            if error:
+                raise error
+            else:
+                return query_result
 
         if (self.__ansible.check_mode.is_active and
             self._may_run_in_check_mode(action_name, args)):
@@ -88,7 +93,7 @@ class Subaction(object):
         """Obsolete, please use ansible_api.AnsibleActions.check_mode.is_active instead."""
         return self.__ansible.check_mode.is_active
 
-    def change (self, action_name, args, update_result=None):
+    def change (self, action_name, args, failed_when=None, update_result=None):
         """Execute an effectful Ansible sub-action.
 
         If check mode is active (i.e. ansible-playbook --check), nothing
@@ -97,6 +102,8 @@ class Subaction(object):
 
         :param action_name: Ansible module name to use
         :param args: Dict with arguments to give to module
+        :param failed_when: An optional function that takes the result and returns
+            a truthy value iff the action failed
         :param update_result: Obsolete, set the `result` object property instead
 
         :return: The Ansible result dict for the underlying action if update_result was None;
@@ -108,19 +115,46 @@ class Subaction(object):
         else:
             result = self.__ansible.run_action(action_name, args)
 
+        error = self._redress_failure(result, failed_when)
+
         if update_result:
             AnsibleResults.update(update_result, result)
-            self._raise_if_failed(result)
-            return update_result   # Pretty debatable idea - Part of the reason why
-                                   # update_result is obsolete
+            if error:
+                raise error
+            else:
+                return update_result   # Pretty debatable idea - Part of the reason why
+                                       # update_result is obsolete
         else:
             if self.result is not None:
                 AnsibleResults.update(self.result, result)
-            self._raise_if_failed(result)
-            return result
+            if error:
+                raise error
+            else:
+                return result
 
-    def _raise_if_failed(self, result):
-        if 'failed' in result:
-            raise AnsibleActionFail("Subaction failed: %s - Invoked with %s" % (
+    def _redress_failure (self, result, failed_when):
+        """Reset failure in `result` according to `failed_when`
+
+        Returns an error that should be raised soon (after result
+        bookkeeping) if unsuccessful i.e. `failed_when` returns True.
+
+        :param result: An Ansible result dict. Will be mutated in place to
+           delete the "failed" key (if present) if `failed_when`
+           returns falsy
+        :param failed_when: Either None, or a function that takes `result` as
+           the sole parameter and returns a truthy value if there is a failure,
+           or a falsy value if not.
+        """
+        if failed_when is None:
+            failed_when = lambda result: 'failed' in result
+
+        if failed_when(result):
+            return AnsibleActionFail("Subaction failed: %s - Invoked with %s" % (
                 result.get('msg', '(no message)'),
                 result.get('invocation', '(no invocation information)')))
+        else:
+            try:
+                del result['failed']
+            except KeyError:
+                pass
+            return None
